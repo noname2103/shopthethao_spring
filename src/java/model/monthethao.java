088832383f5c860f38635e00f6124927ae60d774
trungminhminh <38715621+trungminhminh@users.noreply.github.com>
2019-04-04 17:32:51 +0700

    fix

--- src/java/model/monthethao.java
+++ src/java/model/monthethao.java
@@ -4,8 +4,13 @@
  * and open the template in the editor.
  */
 package model;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 /**
 /**
  *
@@ -17,4 +22,50 @@ public class monthethao {
     private Integer Mamon ;
     private String TenMon ;
     private String HinhAnh ;
+    
+    @OneToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinColumn(name = "MaMon")
+    private List<monthethao> listmonthethao;
+
+    public monthethao() {
+    }
+
+    public monthethao(Integer Mamon, String TenMon, String HinhAnh) {
+        this.Mamon = Mamon;
+        this.TenMon = TenMon;
+        this.HinhAnh = HinhAnh;
+    }
+
+    public Integer getMamon() {
+        return Mamon;
+    }
+
+    public void setMamon(Integer Mamon) {
+        this.Mamon = Mamon;
+    }
+
+    public String getTenMon() {
+        return TenMon;
+    }
+
+    public void setTenMon(String TenMon) {
+        this.TenMon = TenMon;
+    }
+
+    public String getHinhAnh() {
+        return HinhAnh;
+    }
+
+    public void setHinhAnh(String HinhAnh) {
+        this.HinhAnh = HinhAnh;
+    }
+
+    public List<monthethao> getListmonthethao() {
+        return listmonthethao;
+    }
+
+    public void setListmonthethao(List<monthethao> listmonthethao) {
+        this.listmonthethao = listmonthethao;
+    }
+    
 }
